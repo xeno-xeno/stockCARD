d3a8b3b8ee84ef23b9be97259d075afd2cd8b9f1
xeno-xeno <cmux76@gmail.com>
2025-08-26 13:18:40 +0900

    게임리스트 index 빌드 재수정

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/stockCARD/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'game.html',
+      }
+    }
+  }
 })
